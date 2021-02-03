c2a8ddeaec74fd0c5eb7f3fc5ae31bcb1d7b5e58
Abdul Motaleb (Munna) <71353762+munna4@users.noreply.github.com>
2021-02-03 22:52:32 +0600

    Add files via upload

---
+++ Node.js-Module-on-start-ep-1-main/index.js
@@ -0,0 +1,12 @@
+// const nil=require('./myModules');
+// console.log(nil.nila());
+// console.log(nil.munna());
+// console.log(nil.NilaMunna);
+
+// const {nila}=require('./myModules');
+// console.log(nila());
+
+const nil=require('./myModules');
+console.log(nil.nila());
+console.log(nil.munna());
+console.log(nil.nilaMunna);
---
+++ Node.js-Module-on-start-ep-1-main/myModules.js
@@ -0,0 +1,19 @@
+const nila = () =>{
+return "Nila Was My girlfriends";
+}
+const munna = () =>{
+    return "Munna Was My Boyfriends";
+}
+const nilaMunna = "We are Was Best Friends";
+
+
+// exports.nila=nila;
+// exports.munna=munna;
+// exports.NilaMunna=nilaMunna;
+
+module.exports={
+    nila,
+    munna,
+    nilaMunna
+
+}
